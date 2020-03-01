2c3735f472480f52fb74d0049be3f87dafc425e3
Axanar <steve@iterator.com>
2020-03-01 12:24:48 -0500

    [feature/RetroBoot_1.0]: update files for RetroBoot 1.0

--- payload/Autobleem/rc/launch_rb.sh
+++ payload/Autobleem/rc/launch_rb.sh
@@ -6,13 +6,9 @@
 killall -s KILL showLogo sonyapp ui_menu auto_dimmer pcsx dimmer 
 echo 2 > /data/power/disable
 
-# Do we really need to sync here?
-# sync
 echo Image "$1"
 echo Core "$2"
 
-sh /media/retroarch/retroboot/bin/launch_rom.rfa.sh "$1" "$2"
-rm /tmp/.abload 
-# reinit retroboot
-
-[ -f /media/retroarch/retroboot/bin/init.sh ] && sh /media/retroarch/retroboot/bin/init.sh &
+sh /media/retroarch/retroboot/bin/launch_rfa_rom.sh "$1" "$2"
+rm /tmp/.abload
+usleep 250000 
